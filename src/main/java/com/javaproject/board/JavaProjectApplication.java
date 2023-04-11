38ee8b53f596cd7aae0a036b0b2ffa43e73af77d
HyunWoo <shw7349@naver.com>
2023-04-11 16:45:46 +0900

    첫번쨰 커밋

--- src/main/java/com/javaproject/board/JavaProjectApplication.java
+++ src/main/java/com/javaproject/board/JavaProjectApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaProjectApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(JavaProjectApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProjectApplication.class, args);
+	}
 
 }
