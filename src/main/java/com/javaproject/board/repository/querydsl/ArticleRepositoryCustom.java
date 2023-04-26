1424766e472714de684bde49112469ed84969170
HyunWoo <shw7349@naver.com>
2023-04-27 06:46:36 +0900

    #21 도메인 변겅 및 테스트 세팅 - 해시태그

--- src/main/java/com/javaproject/board/repository/querydsl/ArticleRepositoryCustom.java
+++ src/main/java/com/javaproject/board/repository/querydsl/ArticleRepositoryCustom.java
@@ -1,8 +1,14 @@
 package com.javaproject.board.repository.querydsl;
 
+import com.javaproject.board.domain.Article;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Collection;
 import java.util.List;
 
 public interface ArticleRepositoryCustom {
 
     List<String> findAllDistinctHashtags();
+    Page<Article> findByHashtagNames(Collection<String> hashtagNames, Pageable pageable);
 }
---
+++ src/main/java/com/javaproject/board/repository/querydsl/HashtagRepositoryCustom.java
@@ -0,0 +1,7 @@
+package com.javaproject.board.repository.querydsl;
+
+import java.util.List;
+
+public interface HashtagRepositoryCustom {
+    List<String> findAllHashtagNames();
+}
