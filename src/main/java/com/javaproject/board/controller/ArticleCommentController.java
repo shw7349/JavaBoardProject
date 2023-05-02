f0812c6a3611b34896f302bf6b7f59b05ba47b94
HyunWoo <shw7349@naver.com>
2023-05-02 18:14:22 +0900

    #23 해시태그 검색기능 고도화

--- src/main/java/com/javaproject/board/controller/ArticleCommentController.java
+++ src/main/java/com/javaproject/board/controller/ArticleCommentController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+//생성자 주입
 @RequiredArgsConstructor
 @RequestMapping("/comments")
 @Controller
